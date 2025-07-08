93d08c55aa9ea1d9d1d9a004e470316fb86790f6
Mani <seb.gobbi@gmail.com>
2025-07-08 15:46:48 +0200

    fin html/css

--- app.js
+++ app.js
@@ -48,3 +48,24 @@ typewriter
 .pauseFor(1000)
 
 .start()
+
+
+// Animation contact
+
+const input_fields = document.querySelectorAll('input');
+
+for(let i = 0; i < input_fields.length; i++) {
+
+    let field = input_fields[i];
+    
+
+    field.addEventListener('input', (e) =>{
+        if(e.target.value !== ''){
+            e.target.parentNode.classList.add('animation')
+        } else if (e.target.value == ''){
+            e.target.parentNode.classList.remove('animation')
+        }
+    })
+
+}
+
